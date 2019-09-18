66bed0b6b9701ba04579d3fbd76ec76206c4148e
David Varela <00.varela.david@gmail.com>
2019-09-17 21:59:50 -0700

    Refactor repo handling

--- test/pkg.jl
+++ test/pkg.jl
@@ -136,7 +136,7 @@ temp_pkg_dir() do project_path
         Pkg.gc(;collect_delay=Day(1000))
         @test !isempty(readdir(pkgdir))
 
-        # Setting collect_delay to zero causes it to be reaped immediately, howeveage
+        # Setting collect_delay to zero causes it to be reaped immediately, however
         Pkg.gc(;collect_delay=Second(0))
         @test isempty(readdir(pkgdir))
     end
@@ -506,7 +506,7 @@ end
     temp_pkg_dir() do project_path; cd_tempdir(;rm=false) do tmpdir; with_temp_env(;rm=false) do
         for x in ["x1", "x2", "x3"]
             cp(joinpath(@__DIR__, "test_packages/$x"), joinpath(tmpdir, "$x"))
-            Pkg.develop(Pkg.PackageSpec(url = joinpath(tmpdir, x)))
+            Pkg.develop(Pkg.PackageSpec(path = joinpath(tmpdir, x)))
         end
         Pkg.test("x3")
     end end end
